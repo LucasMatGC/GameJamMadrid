b9ef6e3920a5d185430e4db34ab43aa87c9c67d0
LucasMGC <lucasmatias.goncal@gmail.com>
2021-10-24 18:33:18 +0200

    Fixed movement

--- Assets/Scripting/Sounds/ImpactController.cs
+++ Assets/Scripting/Sounds/ImpactController.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImpactSound : MonoBehaviour
+public class ImpactController : MonoBehaviour
 {
     public AudioSource impactSoundHit1;
     public AudioSource impactSoundHit2;
     public AudioSource impactSoundBox;
+    public Rigidbody player = null;
+    public Character3DControllerV3 characterController;
 
     void OnCollisionEnter(Collision collision)
     {
@@ -45,6 +47,18 @@ public class ImpactSound : MonoBehaviour
                 default:
 
                     break;
+            }
+        }
+
+        if (player != null)
+        {
+            if (player.velocity.magnitude <= 1)
+            {
+                player.velocity = new Vector3(0, 0, 0);
+                player.angularVelocity = new Vector3(0, 0, 0);
+                characterController.brakeForce = characterController.maxBreakForce;
+                characterController.brakeForce = 0;
+
             }
         }
 
